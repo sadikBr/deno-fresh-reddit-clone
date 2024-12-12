c5394ed97f3902767a788cdbd1cd28e2a005393b
sadikBr <sadbrah@gmail.com>
2024-12-12 13:28:43 +0100

    solving mobile styles

--- components/searchForm.tsx
+++ components/searchForm.tsx
@@ -2,30 +2,34 @@ export default function SearchForm() {
   return (
     <form
       method="POST"
-      className="bg-white p-1 rounded-lg flex items-center gap-1 max-w-screen"
+      className="bg-white p-1 rounded-lg flex items-center gap-1"
     >
-      <input
-        type="text"
-        name="searchTerm"
-        placeholder="Search Input"
-        class="p-2 rounded-md"
-      />
-      <select name="sortType" class="p-2 rounded-md" >
-        <option value="hot" class="p-2">HOT</option>
-        <option value="new" class="p-2">NEW</option>
-        <option value="top" class="p-2">TOP</option>
-      </select>
-      <select name="sortPeriod" class="p-2 rounded-md" >
-        <option value="all" class="p-2">ALL</option>
-        <option value="week" class="p-2">WEEK</option>
-        <option value="day" class="p-2">DAY</option>
-      </select>
-      <button
-        type="submit"
-        class="bg-yellow-400 text-white px-6 py-2 rounded-md border border-yellow-300 font-bold hover:bg-yellow-300 transition"
-      >
-        Search
-      </button>
+      <div class="flex flex-col md:flex-row items-center gap-1">
+        <input
+          type="text"
+          name="searchTerm"
+          placeholder="Search Input"
+          class="p-2 w-full rounded-md"
+        />
+        <div class="flex items-center gap-1">
+          <select name="sortType" class="p-2 rounded-md" >
+            <option value="hot" class="p-2">HOT</option>
+            <option value="new" class="p-2">NEW</option>
+            <option value="top" class="p-2">TOP</option>
+          </select>
+          <select name="sortPeriod" class="p-2 rounded-md" >
+            <option value="all" class="p-2">ALL</option>
+            <option value="week" class="p-2">WEEK</option>
+            <option value="day" class="p-2">DAY</option>
+          </select>
+          <button
+            type="submit"
+            class="bg-yellow-400 text-white px-6 py-2 rounded-md border border-yellow-300 font-bold hover:bg-yellow-300 transition"
+          >
+            Search
+          </button>
+        </div>
+      </div>
     </form>
   );
 }
